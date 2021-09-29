e1fa04325609b33748184febb4f0184f85895c14
Theodor René Carlsen <theodor.rene@gmail.com>
2021-09-29 15:57:07 +0200

    refactoring. TODO merge user agents

--- main.py
+++ main.py
@@ -1,36 +1,19 @@
 from neo4j import GraphDatabase
 from string import ascii_uppercase
 from sys import argv
-
-debug = True
-
-
-class HelloWorldExample:
-
-    def __init__(self, uri, user, password):
-        self.driver = GraphDatabase.driver(uri, auth=(user, password))
-
-    def close(self):
-        self.driver.close()
-
-    def print_greeting(self, message):
-        with self.driver.session() as session:
-            greeting = session.write_transaction(
-                self._create_and_return, message)
-            print(greeting)
-
-    @staticmethod
-    def _create_and_return(tx, message):
-        result = tx.run("CREATE (a:Greeting) "
-                        " SET a.message = $message "
-                        " RETURN a.message +', from node' + id(a)", message=message)
-        return result.single()[0]
+from config import config as c
 
 
-def insert_line_neo4j(tx, parsed_tuple, hourly_counts):
+def insert_line_neo4j(tx, parsed_tuple, hourly_counts)->None:
+    """
+    Actually insert the data
+    :param parsed_tuple: A parsed tuple
+    :param hourly_counts: [(hour,count)] List of tuples with hourly pageviews
+    :return: None, only sideffects
+    """
     assert len(parsed_tuple) == 5
-    (wiki_code, article_title, page_id, daily_total, _) = parsed_tuple
-    if debug:
+    (wiki_code, article_title, page_id, _, _) = parsed_tuple
+    if c["debug"]:
         print("INSERTING")
     for (hour, count) in hourly_counts:
         tx.run("CREATE (a: PageView )"
@@ -40,8 +23,23 @@ def insert_line_neo4j(tx, parsed_tuple, hourly_counts):
                "SET a.timestamp=$hour "
                "SET a.count=$count ", wiki_code=wiki_code, article_title=article_title, page_id=page_id, hour=hour, count=count)
 
+def generate_ssv_for_line(parsed_tuple, hourly_counts):
+    """
+    Converts our originally parsed tuple into a list of strings with space separated values using hourly_counts
+    """
+    (wiki_code, article_title, page_id, _, _) = parsed_tuple
+    data = []
+    for (hour, count) in hourly_counts:
+        data.append(" ".join([wiki_code, article_title, page_id, str(hour), str(count)]))
+    return data
+
 
 def parse_hourly_counts(tuple):
+    """
+    Convert hourly counts into list of tuples
+    :param tuple: parsed tuple with length 5
+    :returns: [(hour,count)] the hour and the number of hits for that hour in a tuple
+    """
     (_, _, _, _, hourly_counts) = tuple
     alphabet = list(ascii_uppercase)
 
@@ -67,29 +65,44 @@ def parse_hourly_counts(tuple):
 
 
 def parse_line(line):
+    """
+    Parse a line from the file. 
+    :param line: A line to be parsed
+    :returns: None, if line is unparsable
+    :returns: Tuple with lenght 5 if parsable
+    """
     try:
         (wiki_code, article_title, page_id,
          daily_total, hourly_counts) = line.strip().split(" ")
-        #return (wiki_code, article_title, page_id, daily_total, hourly_counts) if wiki_code == "no.wikipedia" else None
-        return (wiki_code, article_title, page_id, daily_total, hourly_counts) 
+        predicates = []
+        if c["only_norway"]:
+            predicates.append(wiki_code == "no.wikipedia")
+        if c["only_pages"]:
+            predicates.append(not article_title.startswith("Fil:"))
+        return (wiki_code, article_title, page_id, daily_total, hourly_counts) if all(predicates) else None
     except:
         return
 
 
-def do_job(tx, filename):
+def do_job(filename):
     with open(filename, "r") as f:
+        if not c["dry_run"]:
+            driver = GraphDatabase.driver(c["uri"], auth=(c["user"], c["password"]))
+
         for line in f:
             parsed = parse_line(line)
             if parsed:
                 hourly_counts = parse_hourly_counts(parsed)
-                insert_line_neo4j(tx, parsed, hourly_counts)
-            elif debug:
+                if c["dry_run"]:
+                    ssv = generate_ssv_for_line(parsed,hourly_counts)
+                    [print(el) for el in ssv]
+                else:
+                    with driver.session() as s:
+                        s.write_transaction(insert_line_neo4j, parsed, hourly_counts)
+            elif c["debug"]:
                 print("DID NOT PARSE:", line)
 
 
 if __name__ == "__main__":
-    greeter = HelloWorldExample("bolt://localhost:7687", "neo4j", "s3cr3t")
-    driver = greeter.driver
     filename = argv[1]
-    with driver.session() as s:
-        s.write_transaction(do_job, filename)
+    do_job(filename)
