b3f5fd4d60703ce1893631b9f5b4ed66f7239d4b
Theodor René Carlsen <theodor.rene@gmail.com>
2021-10-08 14:59:09 +0200

    add missing configfile

---
+++ config.py
@@ -0,0 +1,9 @@
+config = {
+        "debug":False,
+        "uri":"bolt://localhost:7687",
+        "user": "neo4j",
+        "password":"s3cr3t",
+        "dry_run":True,
+        "only_norway":True,
+        "only_pages":True
+        }
