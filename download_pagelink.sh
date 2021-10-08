0ba3709abaa502d284608cc3536c3a59358d9dec
Theodor René Carlsen <theodor.rene@gmail.com>
2021-10-08 20:02:03 +0200

    add file for downloading pagelinks

---
+++ download_pagelink.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+if [ $# -eq 0 ]; then
+    echo "No date argument given. Format: YYYYMMDD"
+    echo "This data is not updated as often. Here are the possible values:"
+    echo "Sorry for the bad formatting"
+    curl -s https://dumps.wikimedia.org/nowiki/ | grep href
+    exit 1
+fi
+arg1=$1
+curl https://dumps.wikimedia.org/nowiki/${arg1}/nowiki-${arg1}-pagelinks.sql.gz -o pagelinks_${arg1}.sql.gz && gzip -d pagelinks_${arg1}.sql
