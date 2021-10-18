d8bf27bc71b1964c1c56d6f457e4c2f5e77b50bb
Theodor René Carlsen <theodor.rene@gmail.com>
2021-10-18 13:29:25 +0200

    add shortest path helper function for fun. TODO change database password lmao

---
+++ shortest_path.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+if [ $# -eq 0 ]; then
+    echo 'match (n:Page{title:"ARG1"}, (q:Page{title:"ARG2"}), p =shortestPath((n)-[*]->(q)) return p;'
+    echo "Returns shortest path between argument #1 and argument #2"
+    exit 0
+fi
+arg1=$1
+arg2=$2
+str='match (n:Page{title:"'
+str2='"}), (q:Page{title:"'
+str3='"}), p = shortestPath((n) -[*]-> (q)) return p;'
+echo "$str$arg1$str2$arg2$str3" | cypher-shell -u neo4j -p QM6yBEkttU7VWx
