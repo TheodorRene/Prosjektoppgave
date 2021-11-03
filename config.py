a3f030cd59d9bb22c622d1af8cea87fc6fe7de1e
Theodor René Carlsen <theodor.rene@gmail.com>
2021-11-03 13:15:54 +0100

    use env variables instead of hardcoded passwords

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
+import os
 config = {
         "debug":False,
         "uri":"bolt://localhost:7687",
         "user": "neo4j",
-        "password":"QM6yBEkttU7VWx",
+        "password":os.environ["NEO4J_PASS"],
         "dry_run":True,
         "only_norway":True,
         "only_pages":True,
@@ -13,7 +14,7 @@ pagelinks_config = {
     "debug":False,
     "host":"localhost",
     "user":"root",
-    "password":"my-secret-pw",
+    "password":os.environ["MARIADB_ROOT_PASSWORD"],
     "database":"pagelinks-db",
     "dry_run":False,
     "use_pagelinks_complete":True
