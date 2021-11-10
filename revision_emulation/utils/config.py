10428a3e5dbd3d373a68c51bed5518e3493124d0
Eivind Kopperud <eivind.kop@gmail.com>
2021-11-10 15:12:02 +0100

    cypher queries in csv

--- revision_emulation/utils/config.py
+++ revision_emulation/utils/config.py
@@ -1,5 +1,8 @@
 revisions_config = {
     "should_create_pagelinks_csv": False,
+    "should_create_revisions_csv": False,
+    "should_create_revision_time_intervals_csv": False,
+    "should_create_cypher_queries_csv": True,
     "revisions_minimum": 2,
     "revisions_maximum": 5,
     "link_gain_probability": 0.001,
