815c5a898140a62f7c538b20097f1c2e35f33531
Theodor René Carlsen <theodor.rene@gmail.com>
2021-11-21 18:47:34 +0100

    Add possible decorators for performance testing
    When timing our functions these helper functions might come in handy.
    Either used manually or as a decorator. I do think we have to do it
    manually at times so its easier to follow the code.

---
+++ perf_time_functions.py
@@ -0,0 +1,40 @@
+from time import perf_counter
+
+repeats = 10
+
+def time_func_avg(func):
+    def wrapFunc(*args, **kwargs):
+        sum = 0 
+        for _ in range(repeats):
+            start = perf_counter()
+            func(*args, **kwargs)
+            stop = perf_counter()
+            sum += (stop-start)
+        print("Average: ", func.__name__, str(sum/repeats))
+    return wrapFunc
+
+        
+def time_func(func):
+    def wrapFunc(*args, **kwargs):
+        start = perf_counter()
+        res = func(*args, **kwargs)
+        stop = perf_counter()
+        print(func.__name__, str(stop-start))
+        return res
+    return wrapFunc
+
+
+@time_func_avg
+def gurba(hei):
+    return [hei for _ in range(1000)]
+
+def gurba2(hei):
+    return [hei for _ in range(1000)]
+
+gurba("hei")
+
+time_func(gurba2)("hei")
+time_func_avg(gurba2)("hei")
+
+
+
