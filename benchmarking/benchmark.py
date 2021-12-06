3f1cd29c52a40fecc3423baea5618bb4a9e4f3be
Theodor René Carlsen <theodor.rene@gmail.com>
2021-12-06 15:02:24 +0100

    add q2

--- benchmarking/benchmark.py
+++ benchmarking/benchmark.py
@@ -1,9 +1,9 @@
-from config import influx_config
+from config import influx_config, config as neo_config
 from datetime import datetime
 from perf_time_functions import time_func_avg, time_func
 
-from influxdb_client import InfluxDBClient, Point
-from influxdb_client.client.write_api import SYNCHRONOUS
+from influxdb_client import InfluxDBClient
+from py2neo import Graph
 
 from queries import *
 
@@ -26,18 +26,32 @@ def getInfluxClient():
         org=influx_config["org"]
         )
 
-queries = [Q1_neo, Q1_influx]
+def getNeo4jDriver():
+    return Graph(
+            neo_config["uri"],
+            auth=(neo_config["user"], neo_config["password"]))
+
+
+Q1_page_id=123
 
 @time_func_avg
-def exe_Q1_neo(query_api):
-    result = query_api.query(org=org,  query=Q1_influx, params={"page_id":123,
-                                                                "timestart": datetime.fromtimestamp(0),
-                                                                "timestop": datetime.now()})
-    return result
+def exe_Q1_influx(query_api):
+    return query_api.query(org=org,  query=Q1_influx, params={"page_id":Q1_page_id,
+        "timestart": datetime.fromtimestamp(0),
+        "timestop": datetime.now()})
+
+
+
+@time_func_avg
+def exe_Q1_neo(g):
+    return g.run(Q1_neo,{"page_id":Q1_page_id})
 
 
 if __name__=="__main__":
     query_api = getInfluxClient().query_api()
-    exe_Q1_neo(query_api)
+    neo4j_driver = getNeo4jDriver()
+
+    exe_Q1_influx(query_api)
+    exe_Q1_neo(neo4j_driver)
 
 
