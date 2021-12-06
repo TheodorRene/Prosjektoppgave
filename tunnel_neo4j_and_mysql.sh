16372f1012fddafe04a6a0207f4a6734fbd98d21
Theodor René Carlsen <theodor.rene@gmail.com>
2021-12-06 12:18:20 +0100

    add influx to tunnel

--- tunnel_neo4j_and_mysql.sh
+++ tunnel_neo4j_and_mysql.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/bash
-ssh -fN -L 7474:localhost:7474 -L 7687:localhost:7687 -L 3306:localhost:3306 dif03.idi.ntnu.no
+ssh -fN -L 7474:localhost:7474 -L 7687:localhost:7687 -L 3306:localhost:3306 -L 8086:localhost:8086 dif06.idi.ntnu.no
