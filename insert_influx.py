fd907dc12d7f52c52ff5f33d027bd8dee78b86ca
Theodor René Carlsen <theodor.rene@gmail.com>
2021-12-07 11:51:35 +0100

    move queries

--- insert_influx.py
+++ insert_influx.py
@@ -94,15 +94,6 @@ def get_average_number_of_hits_for_a_range_multiple_page_ids(page_ids):
        '|> group(columns: ["_measurement"], mode:"by")'
        '|> mean(column: "_value")')
 
-get_page_with_highest_hits_in_a_range = "" + \
-    (f'from(bucket: "{bucket}")'
-       '|> range(start: timestart, stop: timestop)'
-       '|> filter(fn: (r) => r["_measurement"] == "pageview")'
-       '|> filter(fn: (r) => r["_field"] == "hits")'
-       '|> sum(column: "_value")'
-       '|> mean(column: "_value")'
-       '|> highestMax(n: 2, groupColumns: ["_value"])')
-
 
 get_sliding_window_for_page = "" + \
     (f'from(bucket: "{bucket}")'
