71df208c16cb32ea9d00f27b1ce3402a312c1971
Eivind Kopperud <eivind.kop@gmail.com>
2021-12-09 13:06:56 +0100

    feat: q6 (#15)
    * feat: q6
    Co-authored-by: Theodor René Carlsen <36312737+TheodorRene@users.noreply.github.com>

--- benchmarking/utils.py
+++ benchmarking/utils.py
@@ -1,5 +1,10 @@
+from typing import List
+
 def parse_multiple_id_regex(ids):
     """Parses ids [x,y,x] to the form "/^(x|y|z)$/"""
     string_ids = [str(id) for id in ids]
     pipe_separated_ids = "|".join(string_ids)
     return f"/^({pipe_separated_ids})$/"
+
+def get_revision_intervals(timestamps:List[str]) -> List[tuple]:
+    return [(timestamps[i], timestamps[i+1]) for i in range(0, len(timestamps)-1)]
